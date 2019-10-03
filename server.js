29537c63c8dccb28b804a19f040a250637cb643d
Mateen Kazia <mateencode@gmail.com>
2019-10-02 21:11:49 -0700

    prepping for deployment

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const connectDB = require("./config/db");
 const cors = require("cors");
+const path = require("path");
 
 const app = express();
 
@@ -11,10 +12,18 @@ connectDB();
 app.use(cors());
 app.use(express.json({ extended: false }));
 
-app.get("/", (req, res) => res.send("API Running"));
-
 // Define Routes
 app.use("/api/cards", require("./routes/api/cards"));
+
+// Serve static assets in production
+if (process.env.NODE_ENV === "production") {
+  // Set static folder
+  app.use(express.static("client/build"));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
 
 const PORT = process.env.PORT || 5000;
 
